ae36873965b0a3cedc1fab2be1422996473a72f0
carlos reyna <pinduestuar7@gmail.com>
2025-10-23 13:06:31 -0400

    agregar url al cors

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,11 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','storage/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://rally-front.netlify.app','http://localhost:9000', 'http://127.0.0.1:9000',],
+    'allowed_origins' => ['https://rally-front.netlify.app','http://localhost:9000', 'http://127.0.0.1:9000','https://rally2025.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
