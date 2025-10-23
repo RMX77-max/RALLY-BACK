7ab72017e3994084e40c5c1c0dae761e2d1cbe3c
carlos reyna <pinduestuar7@gmail.com>
2025-10-23 18:13:04 -0400

    pre-deploy: pgsql config, cors, seeder admin, composer

--- database/migrations/2025_10_05_192612_add_role_to_users_table.php
+++ database/migrations/2025_10_05_192612_add_role_to_users_table.php
@@ -4,23 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
-        if (! Schema::hasColumn('users', 'role')) {
-            Schema::table('users', function (Blueprint $table) {
+return new class extends Migration {
+    public function up(): void {
+        Schema::table('users', function (Blueprint $table) {
+            if (!Schema::hasColumn('users', 'role')) {
                 $table->string('role')->default('user');
-            });
-        }
+            }
+        });
     }
-
-    public function down(): void
-    {
-        if (Schema::hasColumn('users', 'role')) {
-            Schema::table('users', function (Blueprint $table) {
+    public function down(): void {
+        Schema::table('users', function (Blueprint $table) {
+            if (Schema::hasColumn('users', 'role')) {
                 $table->dropColumn('role');
-            });
-        }
+            }
+        });
     }
 };
